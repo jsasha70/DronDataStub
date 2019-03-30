7aa6cdae9a39f3639d1a867f1c8d1e263c472299
Aleksandr Yanovich <jsasha70@gmail.com>
2019-03-30 23:52:45 +0300

    1

---
+++ src/drondatastub/Main.java
@@ -0,0 +1,15 @@
+package drondatastub;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DronData dd;
+        long t1 = 0, dt;
+        for (int i = 0; i < 100; i++) {
+            dd = Dron.nextPoint(1);
+            dt = dd.getT() - t1;
+            t1 = dd.getT();
+            System.out.println("" + dd.getLat() + "/" + dd.getLon() + "/" + dd.getH() + " - " + dt);
+        }
+    }
+}
